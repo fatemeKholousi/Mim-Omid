422f074cb4abd2c7c5b0818abf106d66de94781a
fatemekholousi <fatemekholousizaer@gmail.com>
2022-05-02 22:16:33 +0430

    fix: default pic and some styles added

--- application/views/show_registered_info.php
+++ application/views/show_registered_info.php
@@ -5,25 +5,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
+<style>
+    .show-info-page{
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: hsla(89, 43%, 51%, 0.3);
+    }
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s;
+        width: 250px;
+        flex-direction: column;
+        background-color: white;
+        
+    }
+    .card h4{
+        text-align: center;
+    }
 
-<body>
- <h1></h1>
+    .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
 
- <?php
+    .container {
+        padding: 2px 16px;
+    }
+</style>
+</head>
 
-print_r($_POST['first-name']);
-print_r($_POST['last-name']);
-
- print_r($_POST['email']);
-
-
-
-
-
-
-
-
-?>
+<body class="show-info-page">
+<h1>
+                :اطلاعات ثبت شده 
+            </h1>
+    <div class="card">
+    <img src="<?php echo base_url(); ?>assets/images/leaf.jpg" alt="Avatar" style="width:250px; height:250px" />
+      
+        <div class="container">
+           
+            <h4><b><?php
+                    print_r($_POST['first-name']);?> </b>
+                  <b> <?php print_r($_POST['last-name']); ?></b> </h4>
+            <p><?php
+                print_r($_POST['email']);
+                ?></p>
+        </div>
+    </div>
+    <p> </p>
+    <?php
+    ?>
 
 </body>
 
